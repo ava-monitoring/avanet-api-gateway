b9f8a6766df88412fa399c0de8e550f809b580b3
Mattias Jiderhamn <mattias.jiderhamn@avamonitoring.com>
2022-01-05 08:51:11 +0100

    A2-1520 Change log format to logfmt and prefer X-Real-IP or X-Forwarded-For header over client IP

--- router_engine.go
+++ router_engine.go
@@ -2,14 +2,45 @@ package krakend
 
 import (
 	"io"
+	"time"
 
 	botdetector "github.com/devopsfaith/krakend-botdetector/gin"
 	httpsecure "github.com/devopsfaith/krakend-httpsecure/gin"
 	lua "github.com/devopsfaith/krakend-lua/router/gin"
 	"github.com/gin-gonic/gin"
+	"github.com/go-logfmt/logfmt"
 	"github.com/luraproject/lura/config"
 	"github.com/luraproject/lura/logging"
 )
+
+// See https://github.com/gin-gonic/gin/blob/d062a6a6155236883f4c3292379ab94b1eac8b05/logger.go#L143 for original log format
+var customLogFormatter = func(param gin.LogFormatterParams) string {
+	if param.Latency > time.Minute {
+		param.Latency = param.Latency.Truncate(time.Second)
+	}
+	clientIp := param.ClientIP
+	// Extract origin IP set by Nginx Ingress using the PROXY protocol (or by AWS LB at Layer 7)
+	// https://docs.nginx.com/nginx/admin-guide/load-balancer/using-proxy-protocol/
+	// https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/enable-proxy-protocol.html
+	realIp := param.Request.Header.Get("X-Real-IP")
+	forwardedFor := param.Request.Header.Get("X-Forwarded-For")
+	if realIp != "" {
+		clientIp = realIp
+	} else if forwardedFor != "" {
+		clientIp = forwardedFor
+	}
+	output, err := logfmt.MarshalKeyvals("time", param.TimeStamp.Format("2006/01/02 - 15:04:05"),
+		"status", param.StatusCode,
+		"latency", param.Latency,
+		"clientIP", clientIp,
+		"method", param.Method,
+		"path", param.Path,
+		"errorMessage", param.ErrorMessage)
+	if err != nil {
+		return "Error formatting log: " + err.Error()
+	}
+	return string(output) + "\n"
+}
 
 // NewEngine creates a new gin engine with some default values and a secure middleware
 func NewEngine(cfg config.ServiceConfig, logger logging.Logger, w io.Writer) *gin.Engine {
@@ -18,7 +49,11 @@ func NewEngine(cfg config.ServiceConfig, logger logging.Logger, w io.Writer) *gi
 	}
 
 	engine := gin.New()
-	engine.Use(gin.LoggerWithConfig(gin.LoggerConfig{Output: w}), gin.Recovery())
+	engine.Use(gin.LoggerWithConfig(gin.LoggerConfig{
+				Formatter: customLogFormatter, // Use logfmt format
+				Output: w,
+				SkipPaths: []string{"/__health"}, // Do not log health checks
+		}), gin.Recovery())
 
 	engine.RedirectTrailingSlash = true
 	engine.RedirectFixedPath = true
